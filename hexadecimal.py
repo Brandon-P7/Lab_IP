4dc8021116a1255b3112301070e7b8a0ccb7503d
Brandon Paez <paezbrandon007@gmail.com>
2026-08-20 00:02:37 -0600

    UPDATE hexadecimal.py

--- hexadecimal.py
+++ hexadecimal.py
@@ -6,6 +6,7 @@ hexadecimal = ""
 while numero > 0:
     residuo = numero % 16
     if residuo < 10: 
+        
     hexadecimal = str(residuo) + hexadecimal
     numero = numero // 16
 print (hexadecimal)
